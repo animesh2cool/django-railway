c66cc72fcd6cc0abf131cfd024480170872bfff6
animesh2cool <animeshmannaece@gmail.com>
2023-09-14 22:44:20 +0530

    new app

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR,"static"
+]
--- firstproject/views.py
+++ firstproject/views.py
@@ -2,7 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homePage(request):
-    return render(request,"index.html")
+    data={
+        'title': 'Home page',
+        'body': 'created by @nimesh',
+        'clist': ['Java','Python','js'],
+        'student_details': [
+            {'name':'animesh', 'phone':'7003440120'},
+            {'name':'ranu', 'phone':'980012345'},
+            {'name':'sagir', 'phone':'55988612'}
+        ]
+    }
+    return render(request,"index.html",data)
 
 def aboutPage(request):
     return HttpResponse("Welcome  to the about page")
